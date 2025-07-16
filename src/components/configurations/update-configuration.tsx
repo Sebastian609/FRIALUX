593b1f0bf0f9c8ef20bbd45fbc73cdd019b9b656
Sebastian609 <62573264+Sebastian609@users.noreply.github.com>
2025-07-15 19:59:10 -0500

    refactor: Enhance validation and error handling in UpdateConfiguration component
    - Improved validation schema for minValue and maxValue to include range checks and clearer error messages.
    - Added transformations to ensure values are numbers and refined error handling for better user feedback.
    - Refactored component structure for improved readability and maintainability.

--- src/components/configurations/update-configuration.tsx
+++ src/components/configurations/update-configuration.tsx
@@ -4,30 +4,52 @@ import * as z from "zod";
 import Button from "../shared/buttons/Button";
 import { useReadigTypes } from "@/hooks/readingType/get-all-readingType.hook";
 
-const UpdateConfigurationSchema = z.object({
-  minValue: z.number().refine((value) => {
-    const decimalPart = value.toString().split(".")[1];
-    if (!decimalPart) return true;
-    return decimalPart.length <= 2;
-  }, {
-    message: "El número debe tener como máximo 2 decimales."
-    
-  }),
-  maxValue: z.number().refine((value) => {
-    const decimalPart = value.toString().split(".")[1];
-    if (!decimalPart) return true;
-    return decimalPart.length <= 2;
-  }, {
-    message: "El número debe tener como máximo 2 decimales."
-  }),
-  moduleId: z.number(),
-  readingTypeId: z.number(),
-  id: z.number(),
-  isActive: z.boolean(),
-}).refine((data) => data.maxValue > data.minValue, {
-  message: "El máximo debe ser mayor que el mínimo",
-  path: ["maxValue"],
-});
+const UpdateConfigurationSchema = z
+  .object({
+    minValue: z
+      .number()
+      .refine(
+        (value) => {
+          const decimalPart = value.toString().split(".")[1];
+          if (!decimalPart) return true;
+          return decimalPart.length <= 2;
+        },
+        {
+          message: "El número debe tener como máximo 2 decimales.",
+        }
+      )
+      .transform((val) => Number(val))
+      .refine((val) => !isNaN(val), {
+        message: "El valor debe ser un número 😡",
+      }),
+
+    maxValue: z
+      .number()
+      .min(-100, { message: "El valor no puede ser menor a -100" })
+      .max(100, { message: "el valor no puede ser mayor a 100" })
+      .refine(
+        (value) => {
+          const decimalPart = value.toString().split(".")[1];
+          if (!decimalPart) return true;
+          return decimalPart.length <= 2;
+        },
+        {
+          message: "El número debe tener como máximo 2 decimales.",
+        }
+      )
+      .transform((val) => Number(val))
+      .refine((val) => !isNaN(val), {
+        message: "El valor debe ser un número 😡",
+      }),
+    moduleId: z.number(),
+    readingTypeId: z.number(),
+    id: z.number(),
+    isActive: z.boolean(),
+  })
+  .refine((data) => data.maxValue > data.minValue, {
+    message: "El máximo debe ser mayor que el mínimo",
+    path: ["maxValue"],
+  });
 
 type UpdateConfigurationType = z.infer<typeof UpdateConfigurationSchema>;
 
@@ -40,7 +62,11 @@ export default function UpdateConfiguration({
   configuration,
   onSubmit,
 }: UpdateConfigurationProps) {
-  const { register, handleSubmit, formState: { errors } } = useForm<UpdateConfigurationType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UpdateConfigurationType>({
     resolver: zodResolver(UpdateConfigurationSchema),
     defaultValues: configuration,
   });
@@ -52,28 +78,46 @@ export default function UpdateConfiguration({
   const { data } = useReadigTypes();
 
   return (
-    <form onSubmit={handleSubmit(update)} className="max-w-md mx-auto rounded-lg space-y-4">
-      <h2 className="text-lg font-semibold text-gray-800">Actualizar Configuración</h2>
+    <form
+      onSubmit={handleSubmit(update)}
+      className="max-w-md mx-auto rounded-lg space-y-4"
+    >
+      <h2 className="text-lg font-semibold text-gray-800">
+        Actualizar Configuración
+      </h2>
 
       <div>
-        <label htmlFor="readingTypeId" className="block text-sm font-medium text-gray-700">Tipo de lectura:</label>
+        <label
+          htmlFor="readingTypeId"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Tipo de lectura:
+        </label>
         <select
           id="readingTypeId"
-          {...register("readingTypeId")}
+          {...register("readingTypeId", { valueAsNumber: true })}
           className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:ring-blue-500 focus:border-blue-500"
           required
         >
-          {data && data.map((type) => (
-            <option key={type.id} value={type.id}>
-              {type.name} - {type.simbol}
-            </option>
-          ))}
+          {data &&
+            data.map((type) => (
+              <option key={type.id} value={type.id}>
+                {type.name} - {type.simbol}
+              </option>
+            ))}
         </select>
-        {errors.readingTypeId && <p className="text-red-600">{errors.readingTypeId.message}</p>}
+        {errors.readingTypeId && (
+          <p className="text-red-600">{errors.readingTypeId.message}</p>
+        )}
       </div>
 
       <div>
-        <label htmlFor="minValue" className="block text-sm font-medium text-gray-700">Valor mínimo:</label>
+        <label
+          htmlFor="minValue"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Valor mínimo:
+        </label>
         <input
           type="number"
           id="minValue"
@@ -82,11 +126,18 @@ export default function UpdateConfiguration({
           className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:ring-blue-500 focus:border-blue-500"
           required
         />
-        {errors.minValue && <p className="text-red-600">{errors.minValue.message}</p>}
+        {errors.minValue && (
+          <p className="text-red-600">{errors.minValue.message}</p>
+        )}
       </div>
 
       <div>
-        <label htmlFor="maxValue" className="block text-sm font-medium text-gray-700">Valor máximo:</label>
+        <label
+          htmlFor="maxValue"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Valor máximo:
+        </label>
         <input
           type="number"
           id="maxValue"
@@ -95,7 +146,9 @@ export default function UpdateConfiguration({
           className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:ring-blue-500 focus:border-blue-500"
           required
         />
-        {errors.maxValue && <p className="text-red-600">{errors.maxValue.message}</p>}
+        {errors.maxValue && (
+          <p className="text-red-600">{errors.maxValue.message}</p>
+        )}
       </div>
 
       <input type="hidden" {...register("moduleId")} />
@@ -103,7 +156,9 @@ export default function UpdateConfiguration({
       <input type="hidden" {...register("isActive")} />
 
       <div className="pt-4">
-        <Button type="submit" className="w-full">Actualizar</Button>
+        <Button type="submit" className="w-full">
+          Actualizar
+        </Button>
       </div>
     </form>
   );
